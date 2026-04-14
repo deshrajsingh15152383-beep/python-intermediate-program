6cdd91e6b0724c4f73bc6321a1c1172b7dc69666
Deshraj Singh <deshrajsingh15152383@gmail.com>
2026-04-14 23:02:26 +0530

    Add for the triangle checking

---
+++ triangle-or-not.py
@@ -0,0 +1,8 @@
+a=float(input("Enter first angle:"))
+b=float(input("Enter second angle:"))
+c=float(input("Enter third angle:"))
+sum=a+b+c
+if(sum==180):
+    print("Accor. to the angles it is a triangle.")
+else:
+    print("this is not a triangle.")
